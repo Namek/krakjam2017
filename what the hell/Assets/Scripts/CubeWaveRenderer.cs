74b01181cc994b4206e83ddd8b7c6d9d80e07737
paskal007r <paskal007r@gmail.com>
2017-01-20 23:53:04 +0100

    +waveTest

---
+++ what the hell/Assets/Scripts/CubeWaveRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeWaveRenderer : MonoBehaviour {
+    public float areaToCover = 30f;
+    public GameObject cube;
+    public List<Transform> cubeList;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < (int)areaToCover; i++)
+        {
+            Transform temp = Instantiate(cube).transform;
+            temp.position = gameObject.transform.position + Vector3.right * i;
+
+            cubeList.Add(temp);
+        }
+	}
+
+    float getHeight(float x)
+    {
+        return 0;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        foreach (var item in cubeList)
+        {
+            item.transform.position = new Vector3(item.transform.position.x, getHeight(item.transform.position.x), item.transform.position.z);
+        }
+	}
+}
---
+++ what the hell/Assets/Scripts/ProceduralMesh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter))]
+public class ProceduralMesh : MonoBehaviour
+{
+    Mesh mesh;
+    List<Vector3> vertices= new List<Vector3>();
+    int[] triangles;
+    public void Reset()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh= mesh;
+        mesh.MarkDynamic();
+    }
+
+    public void Start()
+    {
+        MakeMesh();
+        UpdateMesh();
+    }
+
+    void MakeMesh() {
+        vertices.Add(Vector3.zero);
+        vertices.Add(Vector3.up);
+        vertices.Add(Vector3.right);
+        triangles = new int[] { 0, 1, 2 };
+
+    }
+
+    void UpdateMesh() {
+        
+        mesh.SetVertices(vertices);
+        mesh.triangles = triangles;
+    }
+}
