1e995e0bd7e3606dbd112ab86a87ffbd09c02130
paskal007r <paskal007r@gmail.com>
2017-01-22 15:04:36 +0100

    +done|

---
+++ what the hell/Assets/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadScene : MonoBehaviour {
+
+    // Use this for initialization
+    public void LoadLevel() {
+        SceneManager.LoadScene(1);
+    }
+}
